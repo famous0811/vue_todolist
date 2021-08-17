2a45bc9e80ba0deb9e88a87457d0997c57446057
allblack <umh0811@naver.com>
2021-08-17 11:54:54 +0900

    test todo

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <input type="text" v-model="text" @keyup.enter="updateTodo" />
+    <button @click="updateTodo">추가</button>
+    <ul>
+      <li v-for="data in todos" :key="data">{{ data }}</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 
+@Options({
+  data() {
+    return {
+      text: "ㅅㄷㄴㅅ",
+      todos: [],
+    };
+  },
+  methods: {
+    updateTodo() {
+      this.todos.push(this.text);
+      this.text = "";
+    },
+  },
+  // created() {
+  //   // alert("test");
+  // },
+})
 export default class Landing extends Vue {}
 </script>
 
